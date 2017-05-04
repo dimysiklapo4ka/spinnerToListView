b70dfdbb916a5018e3bedee542998f05fa159049
dev <bogdanovroman1221@gmail.com>
2017-05-04 16:25:14 +0300

    second commit

--- app/src/main/java/com/example/d1mys1klapo4ka/myspeenerinit/MainActivity.java
+++ app/src/main/java/com/example/d1mys1klapo4ka/myspeenerinit/MainActivity.java
@@ -6,34 +6,45 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity{
 
     private Spinner spinner;
     private ListView listView;
+    private EditText mEtNew;
+    private Button mBtAdd;
 
-//     String[] lvMax = getResources().getStringArray(R.array.max_task);
-//     String[] lvRoma = getResources().getStringArray(R.array.roma_task);
-//     String[] lvDima = getResources().getStringArray(R.array.dima_task);
-
-    //String[] lvMax = new String[]{"Calculator Math.class","BMI","Wiget Sinoptic"};
-    //String[] lvRoma = new String[]{"Calculator","Piano","Money banking"};
-    //String[] lvDima = new String[]{"Calculator programming","China Calendar","Internet brovzer"};
+    private List<String> lvMax ;
+    private List<String> lvRoma ;
+    private List<String> lvDima ;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        lvMax = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.max_task)));
+        lvRoma = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.roma_task)));
+        lvDima = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.dima_task)));
+
         spinner = (Spinner)findViewById(R.id.spinner);
         listView = (ListView)findViewById(R.id.lvListView);
+        mEtNew = (EditText)findViewById(R.id.et_new);
+        mBtAdd = (Button)findViewById(R.id.bt_add);
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-//                listVievToSpinner(position);
+                listVievToSpinner(position);
             }
 
             @Override
@@ -42,33 +53,46 @@ public class MainActivity extends AppCompatActivity{
             }
         });
 
-//        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this,android.R.layout.activity_list_item, lvMax);
-//
-//        listView.setAdapter(listAdapter);
+        final ArrayAdapter<String> btAdapter;
+        btAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,lvMax);
+        listView.setAdapter(btAdapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+
+        mBtAdd.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"" + btAdapter.getCount(),Toast.LENGTH_SHORT).show();
 
+
+//                btAdapter.add("new task");
+                btAdapter.addAll("new task");
+                btAdapter.notifyDataSetChanged();
+                listView.invalidateViews();
+//                listView.notify();
+                Toast.makeText(MainActivity.this,"" + btAdapter.getCount(),Toast.LENGTH_SHORT).show();
             }
         });
+
     }
-//    ArrayAdapter<String> listAdapter;
-//    private void listVievToSpinner(int position){
-//
-//        switch (position){
-//            case 0:
-//                listAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, lvMax);
-//                break;
-//            case 1:
-//                listAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, lvRoma);
-//                break;
-//            case 2:
-//                listAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, lvDima);
-//                break;
-//            default:
-//                break;
-//        }
-//        listView.setAdapter(listAdapter);
-//    }
+
+    private void listVievToSpinner(int position){
+
+        ArrayAdapter<String> listAdapter = null;
+
+        switch (position){
+            case 0:
+                listAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, lvMax);
+                break;
+            case 1:
+                listAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, lvRoma);
+                break;
+            case 2:
+                listAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, lvDima);
+                break;
+            default:
+                break;
+        }
+        listView.setAdapter(listAdapter);
+    }
 }
